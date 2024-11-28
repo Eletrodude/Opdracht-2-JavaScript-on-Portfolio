b5932fe22f7ded5facf921a05a807c12be9574af
Eletrodude <leonardofaleiro81@gmail.com>
2024-11-28 12:56:05 +0100

    Added a cookie notification

--- FileStorage/main.js
+++ FileStorage/main.js
@@ -1,6 +1,17 @@
 
 
-function DarkMode(){
+// function DarkMode(){
+// let  body =document.getElementsByName('body')
+//
+// }
 
 
+function cookie_melding_Accept(){
+    console.log("Clicked Accept")
+    document.getElementById("Cookie-Melding").style.display = "none"
+}
+
+function cookie_melding_Reject(){
+    console.log("Clicked Reject")
+    document.getElementById("Cookie-Melding").style.display = "none"
 }
