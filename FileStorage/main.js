961af5583b8fb7f26c8f818f5ee65dcc119f9020
Eletrodude <leonardofaleiro81@gmail.com>
2024-12-03 08:45:17 +0100

    Added a cookie notification

--- FileStorage/main.js
+++ FileStorage/main.js
@@ -15,3 +15,25 @@ function cookie_melding_Reject(){
     console.log("Clicked Reject")
     document.getElementById("Cookie-Melding").style.display = "none"
 }
+
+
+function DarkMode(){
+  let element = document.body
+    element.classList.toggle('Dark_Mode')
+    if (element.classList.contains('Dark_Mode')) {
+        localStorage.setItem('darkMode', 'enabled');
+    } else {
+        localStorage.setItem('darkMode', 'disabled');
+    }
+}
+
+// Apply the saved dark mode state on page load
+window.onload = function () {
+    if (localStorage.getItem('darkMode') === 'enabled') {
+        document.body.classList.add('Dark_Mode');
+    }
+
+
+
+
+}
